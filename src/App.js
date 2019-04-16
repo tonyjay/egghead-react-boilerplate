cdf7cc7d581ad9f9f92e639c1a0f7c74397ef3ef
U-OCN\JayT <JayT@ORG138464.OCN.ORIGIN.COM.AU>
2019-04-16 13:06:56 +1000

    Setup of the CSS stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
 class App extends React.Component {
+    state = {
+        count: 4
+    }
+
     render() {
-        return <h1>Carbines</h1>
+        return (
+            <div>
+                <h1>Reactionary!</h1>
+                <h2>State: {this.state.count}</h2>
+                <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
+                <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
+            </div>
+        )
     }
 }
 
