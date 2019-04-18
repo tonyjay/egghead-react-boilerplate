1d393c469bd72a71a422305f61e9838a3e6df51b
U-OCN\JayT <tony.jay@gmail.com>
2019-04-18 16:57:07 +1000

    We have a basic unit test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 
+const Warning = React.lazy(() => import("./Warning"));
+
 class App extends React.Component {
   state = {
     count: 4
@@ -18,6 +20,11 @@ class App extends React.Component {
         <h2 className={count > 10 ? "warning" : null}>Count: {count}</h2>
         <button onClick={this.increment}>+</button>
         <button onClick={this.decrement}>-</button>
+        {count > 10 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : null}
       </div>
     );
   }
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -26,7 +26,8 @@ module.exports = {
           ],
           plugins: [
             "react-hot-loader/babel",
-            "@babel/plugin-proposal-class-properties"
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-syntax-dynamic-import"
           ]
         }
       },
