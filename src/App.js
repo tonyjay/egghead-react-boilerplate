7d1b793fc5c60b048c6816470816710cadb46642
U-OCN\JayT <tony.jay@gmail.com>
2019-04-18 16:22:48 +1000

    Now have presets for browser compatibility

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import {hot} from 'react-hot-loader'
+import React from "react";
+import { hot } from "react-hot-loader";
 
 class App extends React.Component {
-    state = {
-        count: 4
-    }
+  state = {
+    count: 4
+  };
 
-    increment = () => this.setState(state => ({count: ++state.count}))
+  increment = () => this.setState(state => ({ count: ++state.count }));
 
-    decrement = () => this.setState(state => ({count: --state.count}))
+  decrement = () => this.setState(state => ({ count: --state.count }));
 
-    render() {
-        const {count} = this.state
-        return (
-            <div>
-                <h1>HotPocket</h1>
-                <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
-                <button onClick={this.increment}>+</button>
-                <button onClick={this.decrement}>-</button>
-            </div>
-        )
-    }
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Carpola</h1>
+        <h2 className={count > 10 ? "warning" : null}>Count: {count}</h2>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+      </div>
+    );
+  }
 }
 
-export default hot(module)(App)
+export default hot(module)(App);
