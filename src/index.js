70feed0c87ed471dea16d54d0b4f16c82635655d
U-OCN\JayT <tony.jay@gmail.com>
2019-04-19 14:51:38 +1000

    We have propTYpes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom'
 import '@babel/polyfill'
 import App from './App'
 import './styles.css'
+import DefaultErrorBoundary from './DefaultErrorBoundary'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
   </React.StrictMode>,
   document.getElementById('app')
 )
